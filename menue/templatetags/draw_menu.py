fd277d8f7d488f92a6af43a8a814cf07da462461
Ryhor <kave7771@outlook.com>
2023-10-04 17:34:39 +0300

    works with names*

--- menue/templatetags/draw_menu.py
+++ menue/templatetags/draw_menu.py
@@ -5,14 +5,14 @@ from menue.models import Menue
 register = template.Library()
 
 
-@register.inclusion_tag('draw_menu.html')
-def draw_menu(url_string):
-    m_item_path = url_string.split("/")
+@register.inclusion_tag('draw_menu.html', takes_context=True)
+def draw_menu(context, url_string):
+    m_item_path = url_string
 
 
     query = (
         f"WITH RECURSIVE x AS ("
-        f"    SELECT id, title, parent_id, url FROM menue_menue WHERE menue_menue.title LIKE '{m_item_path[0]}' "
+        f"    SELECT id, title, parent_id, url FROM menue_menue WHERE menue_menue.title LIKE '{m_item_path}' "
         f"    UNION "
         f"    SELECT m.id, m.title, m.parent_id, m.url FROM menue_menue m INNER JOIN x x1 ON m.parent_id = x1.id "
         f")"
@@ -26,5 +26,5 @@ def draw_menu(url_string):
     for p in shit:
         print(p)
         res.append(p)
-
-    return {'menu_items':res, 'path':m_item_path, 'iteration':0}
+    print(context.get('request').path)
+    return {'menu_items':res, 'path':context.get('request').path.split('/')[1:], 'iteration':0}
--- menue/views.py
+++ menue/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def draw_menu_view(request, menu_path):
-        return render(request, 'menue.html', {'main_menu': menu_path})
+        return render(request, 'menue.html')
 
