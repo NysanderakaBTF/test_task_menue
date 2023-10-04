cb4f1ae5f4dab46cf4799febed5dc462dfdecad1
Ryhor <kave7771@outlook.com>
2023-10-04 16:41:03 +0300

    add readme

--- menue/urls.py
+++ menue/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import draw_menu_view
 
 urlpatterns = [
-    # path('<path:menu_name>/', draw_menu_view, name='draw_menu'),
+
     path('<path:menu_path>/', draw_menu_view, name='draw_menu'),
 ]
